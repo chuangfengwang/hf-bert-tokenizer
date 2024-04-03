a125cd1665ace6610d3160c7dfc627f39082dc31
chuangfengwang <chuangfengwang@163.com>
2024-04-03 14:09:25 +0800

    Update client_thriftpy2.py

--- src/test/python/client_thriftpy2.py
+++ src/test/python/client_thriftpy2.py
@@ -1,16 +1,29 @@
 import os
 
+import thrift_connector.connection_pool as connection_pool
 import thriftpy2
-from thriftpy2.rpc import make_client
+
+from hf_bert_tokenizer import BertTokenizer
 
 interface_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../resources/bert.thrift')
 interface_thrift = thriftpy2.load(interface_path, module_name="hf_bert_tokenizer_thrift")
 
-if __name__ == '__main__':
-    client = make_client(interface_thrift.BertTokenizer, '127.0.0.1', 8080)
+
+def use_thriftpy2_multi_server_test():
+    servers = [('127.0.0.1', 8080)]
+    pool_thrifty2 = connection_pool.RandomMultiServerClient(
+        interface_thrift.BertTokenizer,
+        timeout=5.0,
+        use_limit=1000,
+        servers=servers,
+        connection_class=connection_pool.ThriftPyCyClient
+    )
+
+    # 可以在中途重新设置 servers
+    pool_thrifty2.set_servers(servers)
 
     # 测试联通性
-    pong = client.ping('ping')
+    pong = pool_thrifty2.ping('ping')
     print('ping result:', pong)
 
     # 测试分词接口
@@ -18,7 +31,7 @@ if __name__ == '__main__':
     texts = [s] * 3
 
     param = interface_thrift.TokenizerParam(text_list=texts, wrapper_id=True)
-    response: interface_thrift.TokenizerResult = client.bert_tokenizer(param, truncate_len=0)
+    response: interface_thrift.TokenizerResult = pool_thrifty2.bert_tokenizer(param, truncate_len=0)
 
     words_list = response.tokenizer_word_list
     ids_list = response.tokenizer_id_list
@@ -26,4 +39,62 @@ if __name__ == '__main__':
         print('words:', words)
         print('ids:', ids)
 
-    client.close()
+    pool_thrifty2.clear()
+
+
+def use_thriftpy2_test():
+    pool_thrifty2 = connection_pool.ClientPool(
+        interface_thrift.BertTokenizer,
+        '127.0.0.1',
+        8080,
+        connection_class=connection_pool.ThriftPyCyClient
+    )
+
+    # 测试联通性
+    pong = pool_thrifty2.ping('ping')
+    print('ping result:', pong)
+
+    # 测试分词接口
+    s = "环保工程师,公司主营废气治理、除尘类业务，本人在技术部带领一个小组团队，主要工作内容为： 1.售前技术支持：前期对接客户沟通技术需求点、技术交流、勘察现场收集有效数据； 2.方案设计：根据客户需求及现场情况，组织技术讨论会，评估风险，确认技术点，设计可行的技术方案、图纸，并做成本概算； 3.技术交底：项目进场前对工程部和客户端等进行技术交底； 4.项目验收：项目验收资料编制、汇总，给客户做培训； 5.整个项目生命周期内技术工作跟进、总结； 6.每周对组内成员工作进行汇总并制定工作计划，每月对组内成员进行考核。"
+    texts = [s] * 3
+
+    param = interface_thrift.TokenizerParam(text_list=texts, wrapper_id=True)
+    response: interface_thrift.TokenizerResult = pool_thrifty2.bert_tokenizer(param, truncate_len=0)
+
+    words_list = response.tokenizer_word_list
+    ids_list = response.tokenizer_id_list
+    for words, ids in zip(words_list, ids_list):
+        print('words:', words)
+        print('ids:', ids)
+
+
+def use_native_test():
+    pool_native = connection_pool.ClientPool(
+        BertTokenizer,
+        '127.0.0.1',
+        8080,
+        connection_class=connection_pool.ThriftClient
+    )
+
+    # 测试联通性
+    pong = pool_native.ping('ping')
+    print('ping result:', pong)
+
+    # 测试分词接口
+    s = "环保工程师,公司主营废气治理、除尘类业务，本人在技术部带领一个小组团队，主要工作内容为： 1.售前技术支持：前期对接客户沟通技术需求点、技术交流、勘察现场收集有效数据； 2.方案设计：根据客户需求及现场情况，组织技术讨论会，评估风险，确认技术点，设计可行的技术方案、图纸，并做成本概算； 3.技术交底：项目进场前对工程部和客户端等进行技术交底； 4.项目验收：项目验收资料编制、汇总，给客户做培训； 5.整个项目生命周期内技术工作跟进、总结； 6.每周对组内成员工作进行汇总并制定工作计划，每月对组内成员进行考核。"
+    texts = [s] * 3
+
+    param = interface_thrift.TokenizerParam(text_list=texts, wrapper_id=True)
+    response: interface_thrift.TokenizerResult = pool_native.bert_tokenizer(param, truncate_len=0)
+
+    words_list = response.tokenizer_word_list
+    ids_list = response.tokenizer_id_list
+    for words, ids in zip(words_list, ids_list):
+        print('words:', words)
+        print('ids:', ids)
+
+
+if __name__ == '__main__':
+    use_thriftpy2_multi_server_test()
+    # use_thriftpy2_test()
+    # use_native_test()
